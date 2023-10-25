3c86fd4c1765fef31674a8ced047b3da65961d16
Raiku93 <119341000+Raiku93@users.noreply.github.com>
2023-10-25 10:22:37 -0400

    Hebergement du site sur reseau privé
    Pour l'instant l'adresse est fixe : 192.168.1.158:8000 pour accéder au jeu
    Exécuter dans le terminal la commande python manage.py runserver 192.168.1.158:8000
    La prochaine étape concernant le réseau sera de donner un nom de domaine à notre site : pixelwars.fr

--- PixelWars/PixelWars/settings.py
+++ PixelWars/PixelWars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p!v*b9m6)87n%!ojsp+p_(%be7y@tfy7j^hvg8#g-_r2ha-9^m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.158', 'localhost', '127.0.0.1']
 
 
 CORS_ALLOW_ALL_ORIGINS = False
